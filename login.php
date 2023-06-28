6962c439ada9f81d3cae96b696a0a1254c00a5ff
Carolina Silva <116343758+caroolsz@users.noreply.github.com>
2023-06-28 16:42:17 +0100

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
         // Check user is exist in the database
-        $query    = "SELECT * FROM `users` WHERE utilizador='$utilizador'
+        $query    = "SELECT * FROM `utilizador` WHERE utilizador='$utilizador'
                      AND password='" . md5($password) . "'";
         $result = mysqli_query($con, $query) or die(mysql_error());
         $rows = mysqli_num_rows($result);
